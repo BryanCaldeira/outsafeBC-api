df43eaa51ba76c12433fa77d5bfb6d246706a4d7
Wonnnyo Hamester <wonnyohamester@outlook.com>
2023-10-22 17:11:26 -0700

    temp

--- functions/hazard-report.js
+++ functions/hazard-report.js
@@ -1,6 +1,8 @@
 const { faker } = require("@faker-js/faker");
 const headers = require("../utils/headers");
 
+const PushNotifications = require("@pusher/push-notifications-server");
+
 class HazardReport {
   constructor() {
     const coordinates = faker.location.nearbyGPSCoordinate();
@@ -48,6 +50,43 @@ class HazardReport {
 
 const create = async (_event) => {
   const report = new HazardReport();
+
+  const Pusher = require("pusher");
+
+  const pusher = new Pusher({
+    appId: "1691608",
+    key: "353ae3f7ae29d42e5749",
+    secret: "b393684fbb996abf150a",
+    cluster: "us3",
+    useTLS: true,
+  });
+
+  pusher.trigger("my-channel", "my-event", {
+    message: "hello world",
+  });
+
+  // let beamsClient = new PushNotifications({
+  //   instanceId: "db0b4e69-d055-47b5-a8bf-784a5157b8d6",
+  //   secretKey:
+  //     "40CBBE3DF7615DAC8130477B2A30F515F0AF7E07FE84D6338ACDF654567F9DA7",
+  // });
+
+  // beamsClient
+  //   .publishToInterests(["hello"], {
+  //     web: {
+  //       notification: {
+  //         title: "Hello",
+  //         body: "Hello, world Wonnyo!",
+  //         deep_link: "https://www.pusher.com",
+  //       },
+  //     },
+  //   })
+  //   .then((publishResponse) => {
+  //     console.log("Just published:", publishResponse.publishId);
+  //   })
+  //   .catch((error) => {
+  //     console.log("Error:", error);
+  //   });
 
   return {
     ...headers,
